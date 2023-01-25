f1bbcdaef53121c80b2088e836cf157640be17d9
Igor Silva <igor.silva@neppo.com.br>
2023-01-24 21:52:42 -0300

    Interface estatica mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { StatusBar} from 'react-native';
 import { useFonts,Inter_400Regular,Inter_600SemiBold,Inter_700Bold,Inter_800ExtraBold} from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,15 +18,23 @@ export default function App() {
     );
   } 
 
-  return (
+  return(
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+    </>
+  )
+
+  /*return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </View>
-  );
+    
+  );*/
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#09090A',
@@ -38,4 +47,4 @@ const styles = StyleSheet.create({
     fontSize: 100,
     fontFamily: 'Inter_800ExtraBold',
   }
-});
+})*/;
